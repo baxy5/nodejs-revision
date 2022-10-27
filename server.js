57552ac4660c671db73bfc2b293eabe740159b9e
baxy5 <bakos.soda@gmail.com>
2022-10-27 10:43:23 +0200

    requests and responses

--- server.js
+++ server.js
@@ -1,7 +1,47 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  console.log("Request made");
+  console.log(req.url, req.method);
+
+  // headers
+  res.setHeader("Content-Type", "text/html");
+
+  let path = "./views/";
+
+  switch (req.url) {
+    case "/":
+      path += "index.html";
+      res.statusCode = 200;
+      break;
+    case "/home":
+      res.statusCode = 301;
+      res.setHeader("Location", "/");
+      res.end();
+    case "/about":
+      path += "about.html";
+      res.statusCode = 200;
+      break;
+    case "/about-me":
+      res.statusCode = 301;
+      res.setHeader("Location", "/about");
+      res.end();
+    default:
+      path += "404.html";
+      res.statusCode = 404;
+      break;
+  }
+
+  // sending html
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end();
+    } else {
+      res.write(data);
+      res.end();
+    }
+  });
 });
 
 server.listen(3000, "localhost", () => {
