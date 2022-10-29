9744b9f73f3042359e088f3b7a1cd43f015f0ae9
baxy5 <bakos.soda@gmail.com>
2022-10-29 10:09:23 +0200

    view engine ejs

--- app.js
+++ app.js
@@ -6,19 +6,43 @@ app.listen(3000, () => {
   console.log("server listening on 3000");
 });
 
+app.set("view engine", "ejs");
+
 // routing
 
 app.get("/", (req, res) => {
   //res.send("<p>Hello</p>");
-  res.sendFile("./views/index.html", { root: __dirname });
+  //res.sendFile("./views/index.html", { root: __dirname });
+
+  const blogs = [
+    {
+      title: "Yoshi finds eggs",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "Mario finds stars",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "How to defeat bowser",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+  ];
+
+  res.render("index", { title: "Home", blogs });
 });
 
 app.get("/about", (req, res) => {
   //res.send("<p>Hello about</p>");
-  res.sendFile("./views/about.html", { root: __dirname });
+  //res.sendFile("./views/about.html", { root: __dirname });
+  res.render("about", { title: "about" });
 });
 
-// redirects
+app.get("/blogs/create", (req, res) => {
+  res.render("create", { title: "Create a new blog" });
+});
+
+/* // redirects
 
 app.get("/home", (req, res) => {
   res.redirect("/");
@@ -26,10 +50,11 @@ app.get("/home", (req, res) => {
 
 app.get("/about-us", (req, res) => {
   res.redirect("/about");
-});
+}); */
 
 // 404
 
 app.use((req, res) => {
-  res.status(404).sendFile("./views/404.html", { root: __dirname });
+  //res.status(404).sendFile("./views/404.html", { root: __dirname });
+  res.status(404).render("404", { title: "404" });
 });
