378f9ac674356ed99e2ad72d93949ef7541502c5
baxy5 <bakos.soda@gmail.com>
2022-10-29 09:17:15 +0200

    routing

--- app.js
+++ app.js
@@ -6,8 +6,7 @@ app.listen(3000, () => {
   console.log("server listening on 3000");
 });
 
-
-// routing 
+// routing
 
 app.get("/", (req, res) => {
   //res.send("<p>Hello</p>");
@@ -17,4 +16,20 @@ app.get("/", (req, res) => {
 app.get("/about", (req, res) => {
   //res.send("<p>Hello about</p>");
   res.sendFile("./views/about.html", { root: __dirname });
+});
+
+// redirects
+
+app.get("/home", (req, res) => {
+  res.redirect("/");
+});
+
+app.get("/about-us", (req, res) => {
+  res.redirect("/about");
+});
+
+// 404
+
+app.use((req, res) => {
+  res.status(404).sendFile("./views/404.html", { root: __dirname });
 });
